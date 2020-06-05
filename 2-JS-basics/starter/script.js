94009dc5d508a5899c3297f27de7f11b4590a63c
Alexandra <alexandra.shokhan@gmail.com>
2020-06-05 19:33:50 +0300

    If/else statement

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,17 @@
+// console.log("Hello World!");
+
+let firstName = 'John';
+let civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married');
+} else {
+    console.log(firstName + ' will hopefully marry soon');
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married');
+} else {
+    console.log(firstName + ' will hopefully marry soon');
+}
