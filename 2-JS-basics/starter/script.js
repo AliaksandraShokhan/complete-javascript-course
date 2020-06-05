501299214dd69b6734d9ab93b2406b541c32b7d0
Alexandra <alexandra.shokhan@gmail.com>
2020-06-05 19:45:05 +0300

    Boolean logic

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,17 +1,34 @@
 // console.log("Hello World!");
 
+// let firstName = 'John';
+// let civilStatus = 'single';
+//
+// if (civilStatus === 'married') {
+//     console.log(firstName + ' is married');
+// } else {
+//     console.log(firstName + ' will hopefully marry soon');
+// }
+//
+// var isMarried = true;
+// if (isMarried) {
+//     console.log(firstName + ' is married');
+// } else {
+//     console.log(firstName + ' will hopefully marry soon');
+// }
+
+/*
+***** Boolean logic
+ */
+
 let firstName = 'John';
-let civilStatus = 'single';
+let age = 20;
 
-if (civilStatus === 'married') {
-    console.log(firstName + ' is married');
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
 } else {
-    console.log(firstName + ' will hopefully marry soon');
-}
-
-var isMarried = true;
-if (isMarried) {
-    console.log(firstName + ' is married');
-} else {
-    console.log(firstName + ' will hopefully marry soon');
+    console.log(firstName + ' is a man.');
 }
