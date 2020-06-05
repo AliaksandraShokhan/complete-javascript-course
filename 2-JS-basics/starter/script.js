6c5676ac5195050e47fa4ab4e9e4313657ff36f6
Alexandra <alexandra.shokhan@gmail.com>
2020-06-05 21:11:31 +0300

    Coding Challenge 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -74,24 +74,50 @@ Truthy and Falsy values and equality operators
 // falsy values: undefined, null, 0, '', NaN
 // truthy values: NOT falsy values
 
-let height;
-height = '23';
+// let height;
+// height = '23';
+//
+// if(height || height === 0) {
+//     console.log('Variable is defined');
+// } else {
+//     console.log('Variable is not defined');
+// }
+//
+//
+// if (height == 23) {
+//     console.log('The == operator does type coercion');
+// }
 
-if(height || height === 0) {
-    console.log('Variable is defined');
-} else {
-    console.log('Variable is not defined');
+let teamMike = [89, 120, 103];
+let teamJohn = [116, 95, 123];
+let teamMary = [120, 87, 109];
+
+function average(arr){
+    let x = arr.reduce(function(a,b){
+        return a + b
+    }, 0);
+    return x / arr.length;
 }
 
+let averageMike = average(teamMike);
+let averageJohn = average(teamJohn);
+let averageMary = average(teamMary);
 
-if (height == 23) {
-    console.log('The == operator does type coercion');
+console.log(averageMike, averageJohn, averageMary);
+
+switch (true) {
+    case averageMike > averageJohn && averageMike > averageMary:
+        console.log('Mike wins');
+        break;
+    case averageJohn > averageMike && averageJohn > averageMary:
+        console.log('John wins');
+        break;
+    case averageMary > averageJohn && averageMary > averageMike:
+        console.log('Mary wins');
+        break;
+    default:
+        console.log('It\'s a draw');
 }
-
-
-
-
-
 
 
 
